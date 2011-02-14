b350dad4a1688b3be776d27596761e69e2d75d1a
gv1 <gv1@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2011-02-14 15:15:42 +0000

    Delete unused package.
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/psu/trunk/genlib/java/crawl@15506 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- src/org/genedb/crawl/model/interfaces/QuerySource.java
+++
@@ -1,8 +0,0 @@
-package org.genedb.crawl.model.interfaces;
-
-import org.gmod.cat.OrganismsMapper;
-
-public interface QuerySource {
-	Regions getRegions();
-	OrganismsMapper getOrganisms();
-}
--- src/org/genedb/crawl/model/interfaces/Regions.java
+++
@@ -1,8 +0,0 @@
-package org.genedb.crawl.model.interfaces;
-
-import org.genedb.crawl.CrawlException;
-import org.genedb.crawl.model.Locations;
-
-public interface Regions {
-	Locations locations(String region, int start, int end) throws CrawlException;
-}
