3b403a06ab7665fa34c1bd7a9e53087a7b4e5c3c
Giles Velarde <gv1@sanger.ac.uk>
2011-12-07 18:00:28 +0000

    web service extended to include strain names, added a way to use the java binary that's in the java home if this environment variable is set in the command line crawl

--- src/main/java/org/genedb/crawl/dao/backend/OrganismsDAO.java
+++ src/main/java/org/genedb/crawl/dao/backend/OrganismsDAO.java
@@ -84,12 +84,46 @@ public class OrganismsDAO extends BaseDAO implements org.genedb.crawl.dao.Organi
         return organismsMapper.getOrganismProps(o, cv);
     }
     
+    private String[] trims = new String[] {"strain", ",", "_"};
+    
     private List<Organism> addProps(List<Organism> list) {
         for (Organism organism : list) {
             Property prop = organismsMapper.getOrganismProp(organism, "genedb_misc", "translationTable");
             if (prop != null) {
                 organism.translation_table = Integer.parseInt(prop.value);
             }
+            
+            organism.species = organism.species.trim();
+            
+            if (organism.strain != null) {
+                String strain = organism.strain;
+                
+                int strainPosition = organism.species.indexOf(strain);
+                
+//                System.out.println(String.format("'%s' '%s' '%s' ... (%s)", organism.species, organism.species, organism.strain, strainPosition));
+                
+                if (strainPosition > -1) {
+                    String newSpecies = organism.species.substring(0,strainPosition).trim();
+                    
+                    
+                    for (String trim : trims) {
+//                        System.out.println(String.format("??? '%s' '%s' %s ",newSpecies, trim, newSpecies.endsWith(trim)));
+                        if (newSpecies.endsWith(trim)) {
+                            String oldSpecies = newSpecies;
+                            newSpecies = newSpecies.substring(0, newSpecies.lastIndexOf(trim)).trim();
+//                            System.out.println(String.format(">>> '%s' '%s' -> '%s'", trim, oldSpecies, newSpecies));
+                        }
+                    }
+                    
+                    newSpecies = newSpecies;
+                    
+                    organism.species = newSpecies;
+                }
+                
+                //organism.name = String.format("%s %s %s", organism.genus, organism.species, organism.strain).trim();
+                
+            }
+            
         }
         return list;
     }
