5c45b891ddbb3cd1bdaaf1744a5e80ab1fccfc90
Giles Velarde <gv1@sanger.ac.uk>
2012-01-10 17:17:06 +0000

    added put mapping for the feature table

--- src/main/java/org/genedb/crawl/elasticsearch/index/gff/GFFIndexBuilder.java
+++ src/main/java/org/genedb/crawl/elasticsearch/index/gff/GFFIndexBuilder.java
@@ -1,14 +1,20 @@
 package org.genedb.crawl.elasticsearch.index.gff;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.text.ParseException;
-import java.util.EnumSet;
 
 import org.apache.log4j.Logger;
-import org.elasticsearch.action.admin.cluster.health.ClusterHealthStatus;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.action.admin.indices.exists.IndicesExistsRequest;
+import org.elasticsearch.action.admin.indices.exists.IndicesExistsResponse;
 import org.elasticsearch.action.admin.indices.flush.FlushRequest;
 import org.elasticsearch.action.admin.indices.flush.FlushResponse;
-import org.elasticsearch.action.admin.indices.refresh.RefreshRequest;
+import org.elasticsearch.action.admin.indices.mapping.put.PutMappingResponse;
+import org.elasticsearch.client.IndicesAdminClient;
 import org.genedb.crawl.elasticsearch.index.NonDatabaseDataSourceIndexBuilder;
 import org.genedb.crawl.model.Organism;
 import org.kohsuke.args4j.Option;
@@ -23,10 +29,38 @@ public class GFFIndexBuilder extends NonDatabaseDataSourceIndexBuilder {
 	
 	@Option(name = "-o", aliases = { "--organism" }, usage = "The organism, expressed as a JSON.", required = false)
 	public String organism;
+	
+	public final static String featureMapping = "org/genedb/crawl/model/Feature.json";
 		
 	public void run() throws IOException, ParseException, SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
 		
 		init();
+		
+		IndicesAdminClient indexClient = connection.getClient().admin().indices();
+		
+		// the put mapping below needs an existing index
+		IndicesExistsResponse exists = indexClient.exists(new IndicesExistsRequest(connection.getIndex())).actionGet();
+		if (! exists.exists()) {
+	        CreateIndexResponse created = indexClient.create(new CreateIndexRequest(connection.getIndex())).actionGet();
+	        logger.info("Index set? " + created.acknowledged());
+	        
+	        if (! created.acknowledged())
+	            throw new RuntimeException("Could not create index!");
+	        
+		}
+		
+		// apply the put mapping
+		String featureMappingJson = getMapping(featureMapping);
+        PutMappingResponse mapped = indexClient.preparePutMapping(
+                connection.getIndex())
+                .setSource(featureMappingJson)
+                .setType(connection.getFeatureType())
+                .execute()
+                .actionGet();
+		
+        if (! mapped.acknowledged())
+            throw new RuntimeException("Could not create mapping!");
+        
 		
 		if (gffs != null) {
 			
@@ -36,13 +70,32 @@ public class GFFIndexBuilder extends NonDatabaseDataSourceIndexBuilder {
 			
 			Organism o = getAndPossiblyStoreOrganism(organism);
 			convertPath(gffs,o);
+			
 		}
+		
+		FlushResponse fr = indexClient.flush(new FlushRequest(this.connection.getIndex())).actionGet();
+		logger.info(String.format("Flush! %s failed,  %s successful, %s total", fr.getFailedShards(), fr.getSuccessfulShards(), fr.getTotalShards()));
 		
 		logger.debug("Complete");
 		
-		FlushResponse fr = connection.getClient().admin().indices().flush(new FlushRequest(this.connection.getIndex())).actionGet();
-		logger.info(String.format("Flush! %s failed,  %s successful, %s total", fr.getFailedShards(), fr.getSuccessfulShards(), fr.getTotalShards()));
-		
+	}
+	
+	/*
+	 * Returns the string for a resource file (to be used for mapping).
+	 */
+	private String getMapping(String mapping) throws IOException {
+	    
+	    // fetch the feature mapping definition
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream(mapping);
+        BufferedReader buf = new BufferedReader(new InputStreamReader(is));
+        
+        StringBuffer sb = new StringBuffer();
+        String line = null;
+        while ((line = buf.readLine()) != null) {
+            sb.append(line);
+        }
+        return sb.toString();
+        
 	}
 	
 	public static void main(String[] args) throws Exception {
