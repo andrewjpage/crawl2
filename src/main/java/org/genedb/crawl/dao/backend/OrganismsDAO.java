504200c7af1c53d9e31491ac40e81d1739e6d2a2
Giles Velarde <gv1@sanger.ac.uk>
2012-01-11 13:21:23 +0000

    tidying up of species strain hacking

--- src/main/java/org/genedb/crawl/dao/backend/OrganismsDAO.java
+++ src/main/java/org/genedb/crawl/dao/backend/OrganismsDAO.java
@@ -3,6 +3,7 @@ package org.genedb.crawl.dao.backend;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.genedb.crawl.CrawlException;
 import org.genedb.crawl.mappers.OrganismsMapper;
 import org.genedb.crawl.model.Organism;
@@ -12,6 +13,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class OrganismsDAO extends BaseDAO implements org.genedb.crawl.dao.OrganismsDAO {
+    
+    private static Logger logger = Logger.getLogger(OrganismsDAO.class);
     
     @Autowired
     private OrganismsMapper organismsMapper;
@@ -84,8 +87,6 @@ public class OrganismsDAO extends BaseDAO implements org.genedb.crawl.dao.Organi
         return organismsMapper.getOrganismProps(o, cv);
     }
     
-    private String[] trims = new String[] {"strain", ",", "_"};
-    
     private List<Organism> addProps(List<Organism> list) {
         for (Organism organism : list) {
             Property prop = organismsMapper.getOrganismProp(organism, "genedb_misc", "translationTable");
@@ -95,29 +96,22 @@ public class OrganismsDAO extends BaseDAO implements org.genedb.crawl.dao.Organi
             
             organism.species = organism.species.trim();
             
+            /*
+             * This is a hack to make remove the strain from species name!
+             */
             if (organism.strain != null) {
                 String strain = organism.strain;
                 
                 int strainPosition = organism.species.indexOf(strain);
                 
-//                System.out.println(String.format("'%s' '%s' '%s' ... (%s)", organism.species, organism.species, organism.strain, strainPosition));
-                
                 if (strainPosition > -1) {
                     String newSpecies = organism.species.substring(0,strainPosition).trim();
                     
-                    
-                    for (String trim : trims) {
-//                        System.out.println(String.format("??? '%s' '%s' %s ",newSpecies, trim, newSpecies.endsWith(trim)));
-                        if (newSpecies.endsWith(trim)) {
-                            String oldSpecies = newSpecies;
-                            newSpecies = newSpecies.substring(0, newSpecies.lastIndexOf(trim)).trim();
-//                            System.out.println(String.format(">>> '%s' '%s' -> '%s'", trim, oldSpecies, newSpecies));
-                        }
-                    }
-                    
-                    newSpecies = newSpecies;
+                    logger.warn(String.format("common_name:: '%s', genus::'%s', species:: '%s', strain:: '%s', displaying species as -------> '%s'", 
+                            organism.common_name, organism.genus, organism.species, organism.strain, newSpecies));
                     
                     organism.species = newSpecies;
+                    
                 }
                 
                 //organism.name = String.format("%s %s %s", organism.genus, organism.species, organism.strain).trim();
