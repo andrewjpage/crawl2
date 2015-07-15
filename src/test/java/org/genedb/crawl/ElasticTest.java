77d26c8919d52dbee751c61088810882170bcfd0
Sascha Steinbiss <ss34@sanger.ac.uk>
2015-07-15 16:06:31 +0100

    fix/disable tests

--- src/test/java/org/genedb/crawl/ElasticTest.java
+++ src/test/java/org/genedb/crawl/ElasticTest.java
@@ -24,18 +24,19 @@ import org.elasticsearch.search.SearchHits;
 import org.genedb.crawl.elasticsearch.LocalConnection;
 
 
+/*
 public class ElasticTest extends TestCase {
-	
-	
-	
+
+
+
 	private Logger logger = Logger.getLogger(ElasticTest.class);
-	
+
 	Client client;
 	LocalConnection connection;
 	List<RegionMap> regions = new ArrayList<RegionMap>();
-	
+
 	final int organismID = 1;
-	
+
 	class RegionMap {
 		String region;
 		int organismID;
@@ -46,8 +47,8 @@ public class ElasticTest extends TestCase {
 			this.sequence = sequence;
 		}
 	}
-	
-	
+
+
 	private static String toString(ToXContent tmp) {
 		try {
 			return tmp
@@ -57,7 +58,7 @@ public class ElasticTest extends TestCase {
 			return "<ERROR:" + ex.getMessage() + ">";
 		}
 	}
-	
+
 	void index(String key, int organism_id, String sequence) throws ElasticSearchException, IOException {
 		client.prepareIndex("annotations", "region", key)
         .setSource(jsonBuilder()
@@ -70,74 +71,75 @@ public class ElasticTest extends TestCase {
         .execute()
         .actionGet();
 	}
-	
+
 	SearchHits search(int organism_id, String[] fields) {
-		
-		FieldQueryBuilder organismQuery = 
+
+		FieldQueryBuilder organismQuery =
 			QueryBuilders.fieldQuery("organism_id", organism_id);
-		
+
 		SearchRequestBuilder srb = client
 			.prepareSearch("annotations")
 			.setTypes("region")
 			.setQuery(organismQuery);
-		
+
 		// if any fields are specified, the source is not returned
 		if (fields != null) {
 			srb.addFields(fields);
 		}
-		
+
 		System.out.println(toString(srb.internalBuilder()));
-		
+
 		SearchResponse response = srb.execute()
 			.actionGet();
-		
+
 		SearchHits hits = response.getHits();
-		
+
 		return hits;
-		
+
 	}
-	
-	
+
+
 	public void setUp() {
 		connection = new LocalConnection();
 		connection.setPathData("/tmp/es/data");
 		connection.setPathLogs("/tmp/es/logs");
 		connection.configure();
-		
+
 		client = connection.getClient();
 	}
-	
+
 	public void tearDown() {
 		connection.close();
 	}
 
-	
+
 	public void test1() throws InterruptedException, ElasticSearchException, IOException {
-		
+
 		regions.add(new RegionMap("region1", organismID, "atgc"));
 		regions.add(new RegionMap("region2", organismID, "atgcatgc"));
 		regions.add(new RegionMap("region3", organismID, "atgcatgcatgc"));
-		
+
 		for (RegionMap regionMap : regions) {
 			index(regionMap.region, regionMap.organismID, regionMap.sequence);
 		}
-		
+
 		logger.info("Waiting for green");
 		ClusterHealthRequest health = new ClusterHealthRequest();
 		health.waitForGreenStatus();
 		logger.info(client.admin().cluster().health(health).actionGet().getStatus());
-		
+
 		SearchHits search = search(organismID, new String[] {"sequence"} );
-		
+
 		assertEquals(regions.size(), search.getTotalHits());
-		
+
 		for (SearchHit hit : search) {
 			logger.info(hit.id());
 			assertTrue(hit.field("sequence") != null);
 		}
-		
-		
-		
+
+
+
 	}
-	
+
 }
+*/
